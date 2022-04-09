aea7d8c959562ae356f4c3ef15a366545efb19aa
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-04-09 19:56:32 +0100

    Apr 9

---
+++ JBrains/Essentials/src/VarsTypes.java
@@ -0,0 +1,63 @@
+public class VarsTypes {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+
+        // assign sci notation to a double var
+
+        double val = 5.012E15;
+        double val2 = 5.012E-15;
+
+
+        // cast a double to an int
+
+        double a = 10;
+        int b = (int) a;
+
+
+        // assign 3/2 to a double
+
+        double d = 3.0 / 2;
+
+
+        // declare an int array called myArray
+
+        int myArray[];
+
+
+        // assign 5 ints to myArray
+
+        myArray = new int[5];
+
+
+        // declare and assign a 10 int herArray
+
+        int herArray[] = new int[10];
+
+
+        // assign array with 4 literal values
+
+        int arr[] = {21, 22, 23, 24};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
