02cb47fdb56183550bbc9ec9656c8cf603c4d661
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-11-30 12:38:34 +0000

    List all employees complete - Full stack

---
+++ JavaGuides/my-react-crud/my-react-frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+
+function App() {
+  return (
+    <div className='container'>
+      <ListEmployeeComponent />
+    </div>
+  );
+}
+
+export default App;
--- JavaGuides/my-react-crud/my-springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ JavaGuides/my-react-crud/my-springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package net.javaguides.springboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import net.javaguides.springboot.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 
 }
